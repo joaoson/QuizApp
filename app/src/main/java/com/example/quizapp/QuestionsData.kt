2dbaab955619e17ac2baffc76f6bce32db8a2f9d
carloshobmeier <carloshobmeier@hotmail.com>
2024-11-09 01:03:44 -0300

    acertada associação de imagens para questões de ciências e de história

--- app/src/main/java/com/example/quizapp/QuestionsData.kt
+++ app/src/main/java/com/example/quizapp/QuestionsData.kt
@@ -3,63 +3,70 @@ package com.example.quizapp
 val scienceQuestions = listOf(
     Question(
         questionText = "Qual planeta é conhecido como o 'Planeta Vermelho'?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia1,
         options = listOf("Marte", "Júpiter", "Saturno", "Vênus"),
         correctAnswer = "Marte"
     ),
 
-
     Question(
         questionText = "Qual é o gás mais abundante na atmosfera terrestre?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia2,
         options = listOf("Oxigênio", "Hidrogênio", "Nitrogênio", "Dióxido de carbono"),
         correctAnswer = "Nitrogênio"
     ),
+
     Question(
         questionText = "Qual órgão humano é responsável por bombear o sangue?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia3,
         options = listOf("Cérebro", "Pulmões", "Coração", "Estômago"),
         correctAnswer = "Coração"
     ),
+
     Question(
         questionText = "Qual é o menor componente de um elemento químico?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia4,
         options = listOf("Molécula", "Átomo", "Próton", "Nêutron"),
         correctAnswer = "Átomo"
     ),
+
     Question(
         questionText = "Quem é conhecido como o 'pai da teoria da relatividade'?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia5,
         options = listOf("Isaac Newton", "Albert Einstein", "Nikola Tesla", "Galileu Galilei"),
         correctAnswer = "Albert Einstein"
     ),
+
     Question(
         questionText = "Qual é a unidade de medida da força?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia6,
         options = listOf("Joule", "Watt", "Pascal", "Newton"),
         correctAnswer = "Newton"
     ),
+
     Question(
         questionText = "Qual é a camada mais externa da Terra?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia7,
         options = listOf("Manto", "Crosta", "Núcleo interno", "Núcleo externo"),
         correctAnswer = "Crosta"
     ),
+
     Question(
         questionText = "Qual é o principal componente da chuva ácida?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia8,
         options = listOf("Óxido de enxofre", "Ácido sulfúrico", "Ácido clorídrico", "Ácido nítrico"),
         correctAnswer = "Óxido de enxofre"
     ),
+
     Question(
         questionText = "Qual elemento químico dá a cor verde à maioria das plantas?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia9,
         options = listOf("Carbono", "Magnésio", "Ferro", "Cloro"),
         correctAnswer = "Magnésio"
     ),
+
     Question(
         questionText = "Qual é a principal fonte de energia para a Terra?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.ciencia10,
         options = listOf("A Lua", "O Sol", "O vento", "As marés"),
         correctAnswer = "O Sol"
     )
@@ -68,61 +75,70 @@ val scienceQuestions = listOf(
 val historyQuestions = listOf(
     Question(
         questionText = "Quem foi o imperador francês exilado na ilha de Elba?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia1,
         options = listOf("Luís XIV", "Carlos Magno", "Napoleão Bonaparte", "Luís XVI"),
         correctAnswer = "Napoleão Bonaparte"
     ),
+
     Question(
         questionText = "Qual civilização antiga construiu as pirâmides de Gizé?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia2,
         options = listOf("Astecas", "Maias", "Egípcios", "Sumérios"),
         correctAnswer = "Egípcios"
     ),
+
     Question(
         questionText = "Quem descobriu o Brasil em 1500?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia3,
         options = listOf("Pedro Álvares Cabral", "Cristóvão Colombo", "Vasco da Gama", "Fernão de Magalhães"),
         correctAnswer = "Pedro Álvares Cabral"
     ),
+
     Question(
         questionText = "Em que ano ocorreu a Revolução Francesa?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia4,
         options = listOf("1789", "1776", "1804", "1848"),
         correctAnswer = "1789"
     ),
+
     Question(
         questionText = "Quem foi o líder da União Soviética durante a Segunda Guerra Mundial?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia5,
         options = listOf("Lenin", "Stalin", "Khrushchev", "Trotsky"),
         correctAnswer = "Stalin"
     ),
+
     Question(
         questionText = "Qual tratado encerrou a Primeira Guerra Mundial?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia6,
         options = listOf("Tratado de Versalhes", "Tratado de Paris", "Tratado de Tordesilhas", "Tratado de Viena"),
         correctAnswer = "Tratado de Versalhes"
     ),
+
     Question(
         questionText = "Quem foi o líder pacifista que lutou pela independência da Índia?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia7,
         options = listOf("Jawaharlal Nehru", "Subhas Chandra Bose", "Mahatma Gandhi", "Indira Gandhi"),
         correctAnswer = "Mahatma Gandhi"
     ),
+
     Question(
         questionText = "Em que ano foi declarada a independência dos Estados Unidos?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia8,
         options = listOf("1776", "1789", "1801", "1812"),
         correctAnswer = "1776"
     ),
+
     Question(
         questionText = "Qual civilização foi responsável pela invenção do papel?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia9,
         options = listOf("Romana", "Egípcia", "Chinesa", "Mesopotâmica"),
         correctAnswer = "Chinesa"
     ),
+
     Question(
         questionText = "Quem foi o primeiro presidente dos Estados Unidos?",
-        imageResId = R.drawable.paris,
+        imageResId = R.drawable.historia10,
         options = listOf("Thomas Jefferson", "Abraham Lincoln", "George Washington", "John Adams"),
         correctAnswer = "George Washington"
     )
@@ -135,54 +151,63 @@ val mathQuestions = listOf(
         options = listOf("12", "14", "16", "18"),
         correctAnswer = "12"
     ),
+
     Question(
         questionText = "Qual é o valor de Pi até duas casas decimais?",
         imageResId = R.drawable.paris,
         options = listOf("3.15", "3.14", "3.12", "3.10"),
         correctAnswer = "3.14"
     ),
+
     Question(
         questionText = "Quem é conhecido como o pai da geometria?",
         imageResId = R.drawable.paris,
         options = listOf("Pitágoras", "Euclides", "Arquimedes", "Newton"),
         correctAnswer = "Euclides"
     ),
+
     Question(
         questionText = "Qual é o resultado da equação 2x + 6 = 20?",
         imageResId = R.drawable.paris,
         options = listOf("5", "7", "9", "11"),
         correctAnswer = "7"
     ),
+
     Question(
         questionText = "Qual figura geométrica tem exatamente três lados?",
         imageResId = R.drawable.paris,
         options = listOf("Quadrado", "Triângulo", "Círculo", "Hexágono"),
         correctAnswer = "Triângulo"
     ),
+
     Question(
         questionText = "Qual é a fórmula do Teorema de Pitágoras?",
         imageResId = R.drawable.paris,
         options = listOf("a^2 + b^2 = c^2", "a^2 - b^2 = c^2", "a^2 + c^2 = b^2", "b^2 + c^2 = a^2"),
         correctAnswer = "a^2 + b^2 = c^2"
     ),
+
     Question(
         questionText = "Quantos lados tem um dodecágono?",
         imageResId = R.drawable.paris,
         options = listOf("10", "12", "14", "16"),
         correctAnswer = "12"
     ),
+
     Question(
         questionText = "Qual é a soma dos ângulos internos de um triângulo?",
         imageResId = R.drawable.paris,
         options = listOf("180 graus", "360 graus", "90 graus", "270 graus"),
         correctAnswer = "180 graus"
     ),
+
     Question(
         questionText = "O que é um número primo?",
         imageResId = R.drawable.paris,
         options = listOf("Divisível apenas por 1 e ele mesmo", "Divisível por qualquer número", "Divisível por 1, 2 e ele mesmo", "Nenhum dos anteriores"),
         correctAnswer = "Divisível apenas por 1 e ele mesmo"
     ),
+
     Question(
         questionText = "O que é uma fração?",
         imageResId = R.drawable.paris,
@@ -198,54 +223,63 @@ val geoQuestions = listOf(
         options = listOf("Sahara", "Gobi", "Kalahari", "Ártico"),
         correctAnswer = "Ártico"
     ),
+
     Question(
         questionText = "Qual país é atravessado pela linha do Equador e pelo Trópico de Capricórnio?",
         imageResId = R.drawable.paris,
         options = listOf("Brasil", "Austrália", "Índia", "Egito"),
         correctAnswer = "Brasil"
     ),
+
     Question(
         questionText = "Qual é o rio mais longo do mundo?",
         imageResId = R.drawable.paris,
         options = listOf("Amazonas", "Nilo", "Yangtzé", "Mississipi"),
         correctAnswer = "Nilo"
     ),
+
     Question(
         questionText = "Qual é a capital da Austrália?",
         imageResId = R.drawable.paris,
         options = listOf("Sydney", "Melbourne", "Canberra", "Perth"),
         correctAnswer = "Canberra"
     ),
+
     Question(
         questionText = "Quantos países existem na Europa?",
         imageResId = R.drawable.paris,
         options = listOf("44", "50", "54", "58"),
         correctAnswer = "44"
     ),
+
     Question(
         questionText = "Qual é o maior país do mundo em área?",
         imageResId = R.drawable.paris,
         options = listOf("Estados Unidos", "China", "Rússia", "Canadá"),
         correctAnswer = "Rússia"
     ),
+
     Question(
         questionText = "Qual continente é conhecido como o 'berço da humanidade'?",
         imageResId = R.drawable.paris,
         options = listOf("Ásia", "África", "Europa", "América"),
         correctAnswer = "África"
     ),
+
     Question(
         questionText = "Qual é o ponto mais profundo dos oceanos?",
         imageResId = R.drawable.paris,
         options = listOf("Fossa das Marianas", "Fossa de Java", "Abismo Challenger", "Fossa de Tonga"),
         correctAnswer = "Fossa das Marianas"
     ),
+
     Question(
         questionText = "Qual é a montanha mais alta do mundo?",
         imageResId = R.drawable.paris,
         options = listOf("K2", "Monte Everest", "Monte Kilimanjaro", "Monte Elbrus"),
         correctAnswer = "Monte Everest"
     ),
+
     Question(
         questionText = "Qual é o lago mais profundo do mundo?",
         imageResId = R.drawable.paris,
